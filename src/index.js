ebe9c76ef1cb90e18e5da0f3a1580008a7943b83
Nicholas <nicholas.sidwell@gmail.com>
2020-12-24 12:22:19 -0800

    Update api.compatibleWith to support @quasar/app 2.0

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
-/**
- * Quasar App Extension index/runner script
- * (runs on each dev/build)
- *
- * Docs: https://quasar.dev/app-extensions/development-guide/index-api
- * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
- */
-
 module.exports = function (api) {
   api.compatibleWith('quasar', '^1.5.0')
-  api.compatibleWith('@quasar/app', '^1.1.4')
+  api.compatibleWith('@quasar/app', '^1.1.4 || ^2.0.0')
 
-  // Here we extend /quasar.conf.js, so we can add
-  // a boot file which registers our new UI component;
-  // "extendConf" will be defined below (keep reading the tutorial)
   api.extendQuasarConf(extendConf)
 }
 
